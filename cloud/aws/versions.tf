e873778229220337b0d6b342a3330d1744068370
ranajit jana <ranajitjana@gmail.com>
2023-04-01 04:38:22 +0000

    multiple changes to fix cd and destroy workflow

--- cloud/aws/versions.tf
+++ cloud/aws/versions.tf
@@ -1,9 +1,13 @@
-provider "aws" {
-  region  = "ap-south-1"
-  version = "~> 4.61"
+terraform {
+  backend "s3" {
+  }
 }
 
 terraform {
-  backend "s3" {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.61"
+    }
   }
 }
