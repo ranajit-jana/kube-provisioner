8760c6cbb0eee12092583cec67ce6892f041baad
ranajit jana <ranajitjana@gmail.com>
2023-04-07 16:18:46 +0000

    variable file location corrected

--- cloud/aws/variables.tf
+++ cloud/aws/variables.tf
@@ -1,18 +1,22 @@
 
 variable "node_group_max_size" {
+  type        = number
   description = "eks_managed_node_groups.max_size"
 }
 
 
 variable "node_group_min_size" {
+  type        = number
   description = "eks_managed_node_groups.min_size"
 }
 
 
 variable "node_group_desired_size" {
+  type        = number
   description = "eks_managed_node_groups.desired_size"
 }
 
 variable "node_group_instance_types" {
-  type = list(string)
+  type        = list(string)
+  description = "ec2 instance type"
 }
