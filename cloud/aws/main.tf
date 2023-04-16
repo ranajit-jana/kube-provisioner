721ff8949729de757ebc247f2c07f1e2526e0e46
ranajit jana <ranajitjana@gmail.com>
2023-04-16 15:24:52 +0000

    adding eks node group

--- cloud/aws/main.tf
+++ cloud/aws/main.tf
@@ -41,7 +41,7 @@ module "eks" {
 
   aws_auth_roles = [
     {
-      rolearn  = eks_managed_node_group.iam_role_arn
+      rolearn  = module.eks_managed_node_group.iam_role_arn
       username = "system:node:{{EC2PrivateDNSName}}"
       groups = [
         "system:bootstrappers",
@@ -74,4 +74,33 @@ module "eks" {
 
 
 
+}
+
+module "eks_managed_node_group" {
+  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+
+  name            = "separate-eks-mng"
+  cluster_name    = module.eks.cluster_name
+  cluster_version = module.eks.cluster_version
+
+  subnet_ids                        = module.vpc.private_subnets
+  cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
+  vpc_security_group_ids = [
+    module.eks.cluster_security_group_id,
+  ]
+
+  ami_type = "BOTTLEROCKET_x86_64"
+  platform = "bottlerocket"
+
+  # this will get added to what AWS provides
+  bootstrap_extra_args = <<-EOT
+    # extra args added
+    [settings.kernel]
+    lockdown = "integrity"
+    [settings.kubernetes.node-labels]
+    "label1" = "race"
+    "label2" = "capstone"
+  EOT
+
+  tags = merge(local.tags, { Separate = "eks-managed-node-group" })
 }
