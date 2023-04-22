a18c71de0f506d98d93b679757ed5fdd3c784177
ranajit jana <ranajitjana@gmail.com>
2023-04-22 08:23:11 +0000

    json fix

--- cloud/aws/main.tf
+++ cloud/aws/main.tf
@@ -211,18 +211,16 @@ resource "aws_iam_role" "ng_role" {
   name               = "kube-${var.cluster_name}-NodeGroupRole"
   assume_role_policy = <<POLICY
   {
-    "Version": "2012-10-17"
-    "Statement": [
-      {
-       "Effect": "Allow"
-       "Principal": {
-        "Service":[
-          "ec2.amazonaws.com
-        ]
-       },
-       "Action": "sts:AssumeRole"
-      }
-    ]
+  	"Version": "2012-10-17",
+  	"Statement": [{
+  		"Effect": "Allow",
+  		"Principal": {
+  			"Service": [
+  				"ec2.amazonaws.com"
+  			]
+  		},
+  		"Action": "sts:AssumeRole"
+  	}]
   }
   POLICY
 }
